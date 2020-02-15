e54186be7615e28ac65d78b327b6be86ea0da9a2
Diego Keller <keller.diego@gmail.com>
2020-02-15 10:57:58 -0300

    Lista de itens do menu do restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -8,6 +8,7 @@ import 'rxjs/add/operator/catch'
 import { MEAT_API } from '../app.api'
 import { Restaurant } from "./restaurant/restaurant.model";
 import { ErrorHandler } from '../app.error-handler'
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 
 @Injectable()
@@ -30,6 +31,12 @@ export class RestaurantsService {
 
     getReviewsByRestaurant(id: string): Observable<any> {
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    getMenuByRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
