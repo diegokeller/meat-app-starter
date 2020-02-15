3a3b6fb6e89e10308651756a2d6b34271ecaa727
Diego Keller <keller.diego@gmail.com>
2020-02-15 14:39:08 -0300

    Formulários e validações no fechamento do pedido.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+import { OrderComponent } from './order/order.component'
 
 export const ROUTES = [
     {path: '', component: HomeComponent},
@@ -19,4 +20,5 @@ export const ROUTES = [
             {path: 'reviews', component: ReviewsComponent}
         ]
     },
+    {path: 'order', component: OrderComponent}
 ]
