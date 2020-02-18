bfecffabf161b789531d2ce08315e28b41b6bbf0
Diego Keller <keller.diego@gmail.com>
2020-02-18 13:53:26 -0300

    HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { LocationStrategy, HashLocationStrategy } from '@angular/common'
 
 import { ROUTES } from './app.routes'
 
@@ -46,6 +47,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
     {
       provide: LOCALE_ID, 
       useValue: 'pt-BR'
+    },
+    {
+      provide: LocationStrategy, useClass: HashLocationStrategy
     }
   ],
   bootstrap: [AppComponent]
