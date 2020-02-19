6f70dadf0e484d070eb783312259f69bd69d62fd
Diego Keller <keller.diego@gmail.com>
2020-02-19 11:21:34 -0300

    Implementação de interface CanAtivate no LoggedInGuard

--- src/app/security/loggedin.guard.ts
+++ src/app/security/loggedin.guard.ts
@@ -1,20 +1,30 @@
-import { CanLoad, Route } from "@angular/router";
+import { CanLoad, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Route } from "@angular/router";
 import { Injectable } from "@angular/core";
 import { LoginService } from "./login/login.service";
 
 @Injectable()
-export class LoggedInGuard implements CanLoad {
+export class LoggedInGuard implements CanLoad, CanActivate {
 
     constructor(private loginService: LoginService){}
 
-    canLoad(route: Route): boolean {
+    checkAuthentication(path: string): boolean {
         const loggedIn = this.loginService.isLoggedIn()
 
         if(!loggedIn){
-            this.loginService.handleLogin(`/${route.path}`)
+            this.loginService.handleLogin(`/${path}`)
         }
 
         return loggedIn;
+    }
+
+    canLoad(route: Route): boolean {
+        console.log('Can load')
+        return this.checkAuthentication(route.path)
+    }
+
+    canActivate(activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): boolean {
+        console.log('Can activate ' + activatedRoute.routeConfig.path)
+        return this.checkAuthentication(activatedRoute.routeConfig.path)
     }
 
 }
