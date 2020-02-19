ce4a2c9bf727c3ee8a0bdd85d759fd253152b8b4
Diego Keller <keller.diego@gmail.com>
2020-02-19 09:27:55 -0300

    Login - usando componente de input criado

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
 @Component({
   selector: 'mt-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [Validators.required])
+    })
+
   }
 
 }
