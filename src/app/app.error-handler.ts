cdd5594c08e0fe29558e56f9d3cbeec7776881a8
Diego Keller <keller.diego@gmail.com>
2020-02-19 16:08:16 -0300

    Usando zonas (NgZone) para corrigir problemas de notificação não sendo ocultada.

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
-import { ErrorHandler, Injectable, Injector } from '@angular/core'
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core'
 import { NotificationService } from './shared/messages/notification.service'
 import { LoginService } from './security/login/login.service'
 
@@ -8,7 +8,7 @@ import { LoginService } from './security/login/login.service'
 export class ApplicationErrorHandler extends ErrorHandler {
 
     constructor(private notificationService: NotificationService, 
-        private injector: Injector){
+        private injector: Injector, private zone: NgZone){
         super()
     }
 
@@ -17,17 +17,20 @@ export class ApplicationErrorHandler extends ErrorHandler {
 
             const message: string = response.error.message
 
-            switch(response.status){
-                case 401:
-                    this.injector.get(LoginService).handleLogin()
-                    break;
-                case 403:
-                    this.notificationService.notify(message || 'Não autorizado.')
-                    break;
-                case 404:
-                    this.notificationService.notify(message || 'Recurso não encontrado.')
-                    break;
-            }
+            this.zone.run(() => {
+                switch(response.status){
+                    case 401:
+                        this.injector.get(LoginService).handleLogin()
+                        break;
+                    case 403:
+                        this.notificationService.notify(message || 'Não autorizado.')
+                        break;
+                    case 404:
+                        this.notificationService.notify(message || 'Recurso não encontrado.')
+                        break;
+                }
+            })
+            
         }
         super.handleError(response)
     }
